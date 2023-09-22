b0bfe98e48e5920bd4c44f749a1f55a80ef1c00e
DichiRoma <144662106+DichiRoma@users.noreply.github.com>
2023-09-22 12:43:56 +0300

    Mobs

---
+++ Boar/boar.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var chase = false
+var  speed = 100
+@onready var anim =$AnimatedSprite2D
+var alive = true
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	var player = $"../../Player/Player"
+	var  direction = (player.position - self.position).normalized()
+	if alive == true:
+		if chase == true:
+			velocity.x = direction.x * speed
+			anim.play("Run")
+		else :
+			velocity.x = 0
+			anim.play("Idel")
+		if direction.x < 0:
+			$AnimatedSprite2D. flip_h = false
+		else :
+			$AnimatedSprite2D. flip_h= true
+		
+	move_and_slide()
+
+
+func _on_detector_body_entered(body):
+	if body.name == "Player":
+		chase = true
+
+
+func _on_detector_body_exited(body):
+	if body.name == "Player":
+		chase = false
+
+
+func _on_heat_body_entered(body):
+	if body.name == "Player":
+		heat()
+	
+func heat ():
+		alive = false
+		anim.play("Heat")
+		await anim.animation_finished
+		queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 230.0
+const JUMP_VELOCITY = -400.0	
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -18,7 +17,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
